16e836363a70eb9b56db3186d374dbe46e3d82d7
Leonid Borodajev <Leonid.Borodajev@tptlive.ee>
2025-01-24 16:12:48 +0200

    fix

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+let name = "Leonid";
+
+export default async () => {
+    const response = await fetch('https://rickandmortyapi.com/api/character?page=3');
+    const data = await response.json();
+    const characters = data.results;
+    const pages = [];
+    characters.forEach(character => {
+        let page = new HtmlWebpackPlugin({
+            template: './src/character.njk',
+            filename: `character_${character.id}.html`,
+            templateParameters: {
+                character,
+            }
+        });
+        pages.push(page);
+    });
+    return {
+
+        entry: './src/index.js',
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'public'),
+            },
+            compress: true,
+            port: 9000,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/i,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sassOptions: {
+                                    quietDeps: true,
+                                },
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.njk$/,
+                    use: [
+                        {
+                            loader: 'simple-nunjucks-loader',
+                            options: {}
+                        }
+                    ]
+                },
+                {
+                    test: /\.html$/,
+                    use: [
+                        {
+                            loader: "html-loader"
+                        }
+                    ]
+                }
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.njk',
+                templateParameters: {
+                    name: name,
+                    characters
+                }
+
+            }),
+            new HtmlWebpackPlugin({
+                template: './src/about.njk',
+                filename: 'about.html'
+            }),
+            ...pages
+        ],
+    };
+};
